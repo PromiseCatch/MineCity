fda8f8ca6f03a0f7fa6f77f28538c276f38845f2
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2020-10-18 02:09:30 -0300

    Implement 1.12.2 Protections

---
+++ Forge/1.12.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_12_2/protection/ColorIndustrialCraftListener.java
@@ -0,0 +1,55 @@
+package br.com.gamemods.minecity.forge.mc_1_12_2.protection;
+
+import br.com.gamemods.minecity.api.shape.PrecisePoint;
+import br.com.gamemods.minecity.forge.base.MineCityForge;
+import br.com.gamemods.minecity.forge.base.accessors.entity.base.IEntity;
+import br.com.gamemods.minecity.forge.base.accessors.entity.base.IEntityLivingBase;
+import br.com.gamemods.minecity.forge.base.accessors.world.IWorldServer;
+import br.com.gamemods.minecity.forge.base.protection.industrialcraft.IndustrialCraftProtections;
+import ic2.api.event.ExplosionEvent;
+import ic2.api.event.LaserEvent;
+import net.minecraft.util.EntityDamageSourceIndirect;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class ColorIndustrialCraftListener extends IndustrialCraftProtections {
+    public ColorIndustrialCraftListener(MineCityForge mod)
+    {
+        super(mod);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onLaserHitsEntity(LaserEvent.LaserHitsEntityEvent event) {
+        if (onEntityDamage(
+                (IEntity) event.hitEntity,
+                (new EntityDamageSourceIndirect("arrow", event.lasershot, event.owner)).setProjectile(),
+                event.power,
+                false
+        )) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onLaserHitsBlock(LaserEvent.LaserHitsBlockEvent event) {
+        if (onLaserHitsBlock(
+                (IEntity) event.lasershot,
+                (IWorldServer) event.getWorld(),
+                event.pos.getX(), event.pos.getY(), event.pos.getZ()
+        )) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onExplosionIC2(ExplosionEvent event) {
+        if (onExplosionIC2(
+                (IEntity) event.entity,
+                new PrecisePoint(event.pos.x, event.pos.y, event.pos.z), event.power,
+                (IEntityLivingBase) event.igniter,
+                event.rangeLimit, event.radiationRange
+        )) {
+            event.setCanceled(true);
+        }
+    }
+}
---
+++ Forge/1.12.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_12_2/protection/ColorSnapshotHandler.java
@@ -0,0 +1,43 @@
+package br.com.gamemods.minecity.forge.mc_1_12_2.protection;
+
+import br.com.gamemods.minecity.forge.base.accessors.block.IBlockSnapshot;
+import br.com.gamemods.minecity.forge.base.protection.SnapshotHandler;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.BlockSnapshot;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class ColorSnapshotHandler implements SnapshotHandler {
+    @SuppressWarnings("unchecked")
+    public void restore(List<IBlockSnapshot> changes) {
+        HashSet<BlockPos> restored = new HashSet<>();
+        for (BlockSnapshot snapshot : (List<BlockSnapshot>) (List) changes) {
+            BlockPos snapPos = snapshot.getPos();
+            if (restored.contains(snapPos)) continue;
+
+            World world = snapshot.getWorld();
+            world.restoringBlockSnapshots = true;
+            snapshot.restore(true, false);
+            world.restoringBlockSnapshots = false;
+            restored.add(snapPos);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void send(List<IBlockSnapshot> changes) {
+        for (BlockSnapshot snapshot : (List<BlockSnapshot>) (List) changes) {
+            int flag = snapshot.getFlag();
+            World world = snapshot.getWorld();
+            IBlockState oldBlock = snapshot.getReplacedBlock();
+            IBlockState newBlock = world.getBlockState(snapshot.getPos());
+            //noinspection ConstantConditions
+            if (newBlock != null && !(newBlock.getBlock().hasTileEntity(newBlock)))
+                newBlock.getBlock().onBlockAdded(world, snapshot.getPos(), newBlock);
+
+            world.markAndNotifyBlock(snapshot.getPos(), null, oldBlock, newBlock, flag);
+        }
+    }
+}
